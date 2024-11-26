ba90f039480b678e8a05448932f8648b63cadaee
huydoan961z <144944736+huydoan961z@users.noreply.github.com>
2024-11-26 12:16:56 +0700

    still study

--- src/server.js
+++ src/server.js
@@ -6,6 +6,8 @@ const port = process.env.PORT || 3001
 const configViewEngine = require('./config/viewEngines')
 
 const webRoutes = require('./routes/web')
+
+const mysql = require('mysql2')
 console.log(process.env) //run the .env
 
 //config template engine
@@ -14,6 +16,29 @@ configViewEngine(app);
 //config static files
 // take the img from static 
 
+
+//TEST CONNECTION 
+
+// Create the connection to database
+const connection = mysql.createConnection({
+    host: 'localhost',
+    port: 3307,
+    user: 'root',
+    password: '123456',
+    database: 'hoidanit',
+});
+
+// A simple SELECT query
+try {
+    const [results, fields] = connection.query(
+        'SELECT * FROM Users u '
+    );
+
+    console.log(results); // results contains rows returned by server
+    console.log(fields); // fields contains extra meta data about results, if available
+} catch (err) {
+    console.log(err);
+}
 
 
 //khai bao route o routes
