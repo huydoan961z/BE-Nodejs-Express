66a765d840cbf4c595e11e7dac39974df29b24db
huydoan961z <144944736+huydoan961z@users.noreply.github.com>
2024-11-28 15:37:34 +0700

    use mongo

--- src/config/database.js
+++ src/config/database.js
@@ -1,15 +1,55 @@
 require('dotenv').config();
 const mysql = require('mysql2/promise');
+const mongoose = require('mongoose');
 
-const connection = mysql.createPool({
-    host: process.env.DB_HOST,
-    port: process.env.DB_PORT,
-    user: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME,
-    waitForConnections: true,
-    connectionLimit: 10,
-    queueLimit: 0,
-});
+// const connection = mysql.createPool({
+//     host: process.env.DB_HOST,
+//     port: process.env.DB_PORT,
+//     user: process.env.DB_USER,
+//     password: process.env.DB_PASSWORD,
+//     database: process.env.DB_NAME,
+//     waitForConnections: true,
+//     connectionLimit: 10,
+//     queueLimit: 0,
+// });
 
+//use moongoes
+
+
+
+
+var dbState = [{
+        value: 0,
+        label: "disconnected"
+    },
+    {
+        value: 1,
+        label: "connected"
+    },
+    {
+        value: 2,
+        label: "connecting"
+    },
+    {
+        value: 3,
+        label: "disconnecting"
+    }
+];
+
+
+
+const connection = async (req, res) => {
+
+    try {
+        const option = {
+            user: process.env.DB_USER,
+            pass: process.env.DB_PASSWORD
+        }
+        await mongoose.connect(process.env.DB_HOST, option);
+        const state = Number(mongoose.connection.readyState);
+        console.log(dbState.find(f => f.value == state).label, "to db");
+    } catch (error) {
+        handleError(error);
+    }
+}
 module.exports = connection;
--- src/server.js
+++ src/server.js
@@ -9,6 +9,7 @@ const configViewEngine = require('./config/viewEngines')
 
 const webRoutes = require('./routes/web')
 const connection = require('./config/database')
+
 console.log(process.env) //run the .env
 
 //config template engine
@@ -20,25 +21,13 @@ app.use(express.urlencoded({
     extended: true
 })) // for data form
 
-//config static files
 // take the img from static 
 
-
-//TEST CONNECTION 
-
-// Create the connection to database
+//call the function
 
 
-// A simple SELECT query
-// let user = [];
-// try {
-//     const [results, fields] = await connection.query('SELECT * FROM Users u');
-//     console.log(results); // results contains rows returned by server
-//     user = results;
-//     console.log(JSON.stringify(user));
-// } catch (err) {
-//     console.error('Error executing query:', err);
-// }
+connection();
+
 
 
 //khai bao route o routes
